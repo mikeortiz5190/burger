da8d4596aa14849d0318eb61d8ce991265a74096
michael Ortiz <michaelortiz@michaels-MacBook-Pro-2.local>
2017-08-05 00:38:57 -0400

    update

--- server.js
+++ server.js
@@ -13,24 +13,6 @@ app.engine("handlebars", exphbs({defaultLayouts:"main"}));
 app.set("view engine", "handlebars");
 var PORT = process.env.PORT || 3011;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "movie_planner_db",
-  port:8889
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-
-});
-
 app.get("/", function(req, res){
   connection.query("SELECT * FROM ___________;", function(err,data){
     res.render("index", {/*MY SQL OBJECT FOR INDEX.HTML {{EACH}} HERE*/});
